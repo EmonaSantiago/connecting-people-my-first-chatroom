c20a943917e937a61e360d103863e247952c23fd
Emona Santiago <emonasantiago1207@gmail.com>
2022-07-05 19:19:37 +0200

    CODE

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*
 https://socket.io/get-started/chat
 */
+const { ifError } = require('assert')
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
@@ -8,12 +9,24 @@ const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 4242
 
+// Start het longpolling proces, geef io mee
+// setInterval(callApi, 2500, io)
+
+// const historySize = 50
+// let history = []
+
 app.use(express.static(path.resolve('public')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+  // io.emit('history', history)
 
   socket.on('message', (message) => {
+    // while (history.length > historySize) {
+    //   history.shift()
+    // }
+    // history.push(message)
+
     io.emit('message', message)
   })
 
@@ -21,6 +34,10 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 })
+
+// function callApi(io) {
+//   io.emit('message', 'whatever')
+// }
 
 http.listen(port, () => {
   console.log('listening on port ', port)
